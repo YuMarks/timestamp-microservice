ef46bd4a0dab68e9fb042f9bcb89501d3dd4381b
ymarks <amy.neal@rocketmail.com>
2016-01-31 22:43:17 +0000

    server.js

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-'use strict';
-
+//var http = require('http');
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
+var moment = require('moment');
+var dateObj = {unix: 0, natural: "string"};
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+function convertNatural(date){
+    dateObj.unix = date;
+    date = date*1000;
+    date = new Date(date);
+    var newDate = moment(date).format("MMMM D, YYYY");
+    dateObj.natural = newDate;
+    
+}
+function convertUnix(date){
+    date = new Date(date);
+    console.log(date);
+    if(date != "Invalid Date"){
+    dateObj.natural = moment(date).format("MMMM D, YYYY");
+    }else{
+        dateObj.natural = null;
+    }
+    var newDate = moment(date).format("x");
+    newDate = newDate/1000;
+    dateObj.unix = newDate;
+    
+}
+app.get('/:query', function(req, res) {
+  var date = req.params.query;
+  if(/[a-z]/i.test(date)){
+    convertUnix(date);
+    
+  }else{
+      convertNatural(date);
+  }
+   var final = JSON.stringify(dateObj);
+   var html = "<p>" + final + "</p>";
+    res.end(html);
+    
 });
+app.listen(port);
